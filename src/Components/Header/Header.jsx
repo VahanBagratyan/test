d820c977877ab366f783c47c010a49cd29b1c516
VahanBagratyan <vahan.bagratyan@gmail.com>
2023-07-03 17:11:26 +0400

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import styles from "./header.module.css";
+
+function Header() {
+  const [isHovered, setIsHovered] = useState(false);
+  return (
+    <div className={styles.root}>
+      <div className={styles.first_layer}>
+        <div className={styles.logo_search_container}>
+          <img src="images/logo.svg" alt="logo" className={styles.logo} />
+          <input className={styles.search} type="text" placeholder="Search" />
+        </div>
+        <div className={styles.navigation_bar}>
+          <div className={styles.country_dropdown}>
+            <div className={styles.flag}>
+              <img
+                src="images/flags/armenia.png"
+                alt="flag"
+                className={styles.flag_img}
+              />
+            </div>
+            <h1 className={styles.dram}>֏</h1>
+            <h1 className={styles.dram}>AMD</h1>
+            <p className={styles.down}></p>
+          </div>
+
+          <div className={styles.country_dropdown}>
+            <div className={styles.flag}>
+              <img
+                src="images/flags/britain.png"
+                alt="flag"
+                className={styles.flag_img}
+              />
+            </div>
+            <h1 className={styles.dram}>English</h1>
+            <p className={styles.down}></p>
+          </div>
+          <p>Sign In</p>
+          <img src="/images/icons/favorite.svg" alt="" className={styles.icon} />
+          <img src="/images/icons/card.svg" alt="" className={styles.icon} />
+        </div>
+      </div>
+      <div className={styles.menu}>
+        <div
+          className={styles.categories}
+          onMouseOver={() => setIsHovered(true)}   onMouseLeave={() => setTimeout(() => setIsHovered(false), 500)}>
+          Categories <p
+            className={
+                isHovered
+                  ? styles.up
+                  : styles.down
+              } 
+            ></p>
+          <div
+        className={
+            isHovered
+              ? styles.category_dropdownOpened
+              : styles.category_dropdownClosed
+          }
+          onMouseLeave={() => setTimeout(() => setIsHovered(false))}
+          ></div>
+        </div>
+
+        <div>Gev</div>
+
+        <div>Gev</div>
+
+        <div>Gev</div>
+
+        <div>Gev</div>
+
+        <div>Gev</div>
+
+        <div>Gev</div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../Components/Header/Header";
+
+function Home() {
+  return (
+    <React.StrictMode>
+      <Header />
+    </React.StrictMode>
+  );
+}
+
+export default Home;
