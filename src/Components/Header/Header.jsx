2a624051cccdf7dbdb1fa1233905de3d9b7bcee6
VahanBagratyan <vahan.bagratyan@gmail.com>
2023-07-12 12:33:15 +0400

    hambaly du es

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,104 +1,28 @@
-import React, { useEffect, useState } from "react";
 import styles from "./header.module.css";
-import axios from "axios";
-import { slide as Menu } from 'react-burger-menu';
-
 //Compoenents
-import MenuBar from "../Menu/Menu"
-import Loader from "../Loader/Loader";
-
+import MenuBar from "../Menu/Menu";
+import Search from "../Search/Search";
+import { useEffect, useState } from "react";
 
 function Header() {
-  const [data, setData] = useState([]);
-  const [dataChild, setDataChild] = useState([]);
-  const [id, setId] = useState(36);
-  const [searchValue, setSearchValue] = useState("");
-  const [searchResult, setSearchResult] = useState([]);
-  const [loader, setLoader] = useState(false);
-  const [searchRec, setSearchRec] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        let response = await axios.post(
-          "https://apishop.yerevan-city.am/api/Category/GetParentCategories",
-          {
-            parentId: 7,
-          }
-        );
-        setData(response.data.data.categories);
-      } catch (error) {
-        console.error(error);
-      }
-    };
 
-    fetchData();
-  }, []);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        let response = await axios.post(
-          "https://apishop.yerevan-city.am/api/Category/GetAllChildren",
-          {
-            parentId: id,
-          },
-          {
-            headers: {
-              Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0ZDMwOWE3Yi04M2E0LTQ2OTUtODJjOC03MTQ4NjcyZWU5ODIiLCJ1bmlxdWVfbmFtZSI6IjE2ODgzNzUwNzUwNTJNbUFQTVlwIiwianRpIjoiM2VhZWRkMjctZDNhNy00MzNkLWI0MGItOGU4ZGViZDA1YjU4IiwiaWF0IjoxNjg4Mzc1MjI4LCJuYmYiOjE2ODgzNzUyMjgsImV4cCI6MTY5NzAxNTIyOCwiaXNzIjoid2ViQXBpIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAyLyJ9._Q0V2d1Ijh6glLBiuHOKaGpjSuy4fZPoDqKjwDco3Ao`,
-            },
-          }
-        );
-        setDataChild(response.data.data.children);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+    // Function to handle window resize event
+    function handleResize() {
+      // Update window width in the state
+      setWindowWidth(window.innerWidth);
+    }
 
-    fetchData();
-  }, [id]);
+    // Attach the resize event listener
+    window.addEventListener('resize', handleResize);
 
-  useEffect(() => {
-    let timeout = setTimeout(async () => {
-      try {
-        if (searchValue.length > 0) {
-          let response = await axios.post(
-            "https://apishop.yerevan-city.am/api/Product/Search",
-            {
-              search: searchValue,
-            }
-          );
-          setLoader(false);
-          setSearchResult(response.data.data.products);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    }, 500);
-
+    // Clean up the event listener on component unmount
     return () => {
-      clearTimeout(timeout);
+      window.removeEventListener('resize', handleResize);
     };
-  }, [searchValue]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        let response = await axios.get(
-          "https://apishop.yerevan-city.am/api/Page/Get?type=2",
-          {
-            headers: {
-              Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0ZDMwOWE3Yi04M2E0LTQ2OTUtODJjOC03MTQ4NjcyZWU5ODIiLCJ1bmlxdWVfbmFtZSI6IjE2ODgzNzUwNzUwNTJNbUFQTVlwIiwianRpIjoiM2VhZWRkMjctZDNhNy00MzNkLWI0MGItOGU4ZGViZDA1YjU4IiwiaWF0IjoxNjg4Mzc1MjI4LCJuYmYiOjE2ODgzNzUyMjgsImV4cCI6MTY5NzAxNTIyOCwiaXNzIjoid2ViQXBpIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAyLyJ9._Q0V2d1Ijh6glLBiuHOKaGpjSuy4fZPoDqKjwDco3Ao`,
-            },
-          }
-        );
-        setSearchRec(response.data.data.specialItems[0].products);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, []);
+  }, []); 
 
   return (
     <div className={styles.root}>
@@ -107,61 +31,7 @@ function Header() {
           <a href="http://localhost:3000" className={styles.logo}>
             <img src="/images/logo.svg" alt="logo" height={100 + "%"} />
           </a>
-          <div className={styles.search}>
-            <input
-              type="text"
-              placeholder="Search"
-              className={styles.search_field}
-              onChange={(evt) => {
-                setSearchValue(evt.target.value);
-                setLoader(true);
-              }}
-            />
-            <div className={styles.search_box}>
-              <div className={styles.search_content}>
-                {searchValue.length <= 0 ? (
-                  <div>
-                    <h1 className={styles.frequently_searched_title}>
-                      Frequently searched
-                    </h1>
-                    <div className={styles.frequently_searched_box}>
-                      {searchRec.map((data) => {
-                        return (
-                          <div className={styles.frequently_searched}>
-                            <img src={data.photo} alt="" height={100 + "%"} />
-                            <div>{data.name}</div>
-                          </div>
-                        );
-                      })}
-                    </div>
-                  </div>
-                ) : searchResult.length > 0 && !loader ? (
-                  searchResult.map((data) => {
-                    return (
-                      <div className={styles.search_item}>
-                        <img
-                          src={data.photo}
-                          alt="item"
-                          className={styles.search_item_img}
-                        />
-                        <div>
-                          <p>
-                            {data.price} <span className={styles.dram}>֏</span>
-                          </p>
-                          <br />
-                          <p>{data.name}</p>
-                        </div>
-                      </div>
-                    );
-                  })
-                ) : searchResult.length <= 0 && !loader ? (
-                  <h1 className={styles.no_element_found}>No Element found</h1>
-                ) : (
-                  <Loader />
-                )}
-              </div>
-            </div>
-          </div>
+          <Search />
         </div>
         <div className={styles.navigation_bar}>
           <div className={styles.country_dropdown}>
@@ -197,10 +67,9 @@ function Header() {
         </div>
       </div>
       <div className={styles.menu_root}>
-        <MenuBar />
+        {windowWidth >= 1000 ? <MenuBar /> : console.log("Vardan")}
       </div>
     </div>
-
   );
 }
 
--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import styles from "./header.module.css";
+import styles from "./menu.module.css";
 import axios from "axios";
 import Loader from "../Loader/Loader";
 
@@ -8,10 +8,6 @@ const MenuBar = () => {
   const [data, setData] = useState([]);
   const [dataChild, setDataChild] = useState([]);
   const [id, setId] = useState(36);
-  const [searchValue, setSearchValue] = useState("");
-  const [searchResult, setSearchResult] = useState([]);
-  const [loader, setLoader] = useState(false);
-  const [searchRec, setSearchRec] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -52,49 +48,6 @@ const MenuBar = () => {
 
     fetchData();
   }, [id]);
-
-  useEffect(() => {
-    let timeout = setTimeout(async () => {
-      try {
-        if (searchValue.length > 0) {
-          let response = await axios.post(
-            "https://apishop.yerevan-city.am/api/Product/Search",
-            {
-              search: searchValue,
-            }
-          );
-          setLoader(false);
-          setSearchResult(response.data.data.products);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    }, 500);
-
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [searchValue]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        let response = await axios.get(
-          "https://apishop.yerevan-city.am/api/Page/Get?type=2",
-          {
-            headers: {
-              Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0ZDMwOWE3Yi04M2E0LTQ2OTUtODJjOC03MTQ4NjcyZWU5ODIiLCJ1bmlxdWVfbmFtZSI6IjE2ODgzNzUwNzUwNTJNbUFQTVlwIiwianRpIjoiM2VhZWRkMjctZDNhNy00MzNkLWI0MGItOGU4ZGViZDA1YjU4IiwiaWF0IjoxNjg4Mzc1MjI4LCJuYmYiOjE2ODgzNzUyMjgsImV4cCI6MTY5NzAxNTIyOCwiaXNzIjoid2ViQXBpIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAyLyJ9._Q0V2d1Ijh6glLBiuHOKaGpjSuy4fZPoDqKjwDco3Ao`,
-            },
-          }
-        );
-        setSearchRec(response.data.data.specialItems[0].products);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   return (
           <div className={styles.menu}>
